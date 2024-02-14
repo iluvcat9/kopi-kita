cca6ab8dfe44c0f89de4d2a15c11359ae19134d2
iluvcat9 <sabrinakartika8090@gmail.com>
2024-02-15 01:04:37 +0700

    initial commit

--- js/script.js
+++ js/script.js
@@ -8,11 +8,9 @@ document.querySelector("#hamburger-menu").onclick = () => {
 // Toogle class active untuk search form
 const searchForm = document.querySelector(".search-form");
 const searchBox = document.querySelector("#search-box");
-
 document.querySelector("#search-button").onclick = (e) => {
   searchForm.classList.toggle("active");
   searchBox.focus();
-  e.preventDefault();
 };
 
 //Toggle class active untuk shopping cart
